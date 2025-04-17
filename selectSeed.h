f4a4fe39f4ecf27884a380b42abcf211f2ed4b7e
Ronnakorn Rattanakornphan <38160239+ronnakorn9@users.noreply.github.com>
2025-04-16 22:06:51 -0400

    add + update MPI process for sending count & cooccur matrix
    HAVE NOT TESTED FOR CORRECTNESS YET

--- selectSeed.h
+++ selectSeed.h
@@ -3,76 +3,79 @@
 #include <set>
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 
 #include "EdgeInfo.h"
+#include "util.h"
 
 typedef EdgeType::IntType NumberType;
 
-void add_count(std::vector<NumberType> &local_count, NumberType node_index, int myrank, int world_size)
-{
-	// local_count is N x N/p matrix
-	// N is total number of node in whole graph, p is number of rank
+// void add_count(std::vector<NumberType> &local_count, NumberType node_index, int myrank, int world_size)
+// {
+// 	// local_count is N x N/p matrix
+// 	// N is total number of node in whole graph, p is number of rank
 
-	// check owner
-	int destination = node_index % world_size;
-	if (destination == myrank)
-	{
-		// this rank own info on this node
-		local_count[node_index]++;
-	}
-	else
-	{
-		// send to others
-		static_assert(std::is_same<NumberType, long>::value || std::is_same<NumberType, int>::value);
-		if (std::is_same<NumberType, long>::value)
-		{
-			// using num type = long
-			MPI_Send(&node_index, 1, MPI_LONG, destination, 0, MPI_COMM_WORLD); // using tag = 0
-		}
-		else if (std::is_same<NumberType, int>::value)
-		{
-			// using num type = int
-			MPI_Send(&node_index, 1, MPI_INT, destination, 0, MPI_COMM_WORLD);
-		}
-		else
-		{
-			std::cout << "NumberType is not a long or int\n";
-			exit(-1);
-		}
-	}
-}
+// 	// check owner
+// 	int destination = node_index % world_size;
+// 	if (destination == myrank)
+// 	{
+// 		// this rank own info on this node
+// 		local_count[node_index]++;
+// 	}
+// 	else
+// 	{
+// 		// send to others
+// 		static_assert(std::is_same<NumberType, long>::value || std::is_same<NumberType, int>::value);
+// 		if (std::is_same<NumberType, long>::value)
+// 		{
+// 			// using num type = long
+// 			MPI_Send(&node_index, 1, MPI_LONG, destination, 0, MPI_COMM_WORLD); // using tag = 0
+// 		}
+// 		else if (std::is_same<NumberType, int>::value)
+// 		{
+// 			// using num type = int
+// 			MPI_Send(&node_index, 1, MPI_INT, destination, 0, MPI_COMM_WORLD);
+// 		}
+// 		else
+// 		{
+// 			std::cout << "NumberType is not a long or int\n";
+// 			exit(-1);
+// 		}
+// 	}
+// }
 
-void add_occurance(std::vector<std::vector<int>> &local_C, NumberType row_index, NumberType col_index, int myrank, int world_size)
-{
-	// check owner
-	int destination = row_index % world_size;
-	if (destination == myrank)
-	{
-		// this rank own info on this row
-		local_C[row_index][col_index]++;
-	}
-	else
-	{
-		// send to others
-		NumberType buffer[2] = {row_index, col_index};
-		static_assert(std::is_same<NumberType, long>::value || std::is_same<NumberType, int>::value);
-		if (std::is_same<NumberType, long>::value)
-		{
-			// using num type = long
-			MPI_Send(buffer, 2, MPI_LONG, destination, 0, MPI_COMM_WORLD); // using tag = 0
-		}
-		else if (std::is_same<NumberType, int>::value)
-		{
-			// using num type = int
-			MPI_Send(buffer, 2, MPI_INT, destination, 0, MPI_COMM_WORLD);
-		}
-		else
-		{
-			std::cout << "NumberType is not a long or int\n";
-			exit(-1);
-		}
-	}
-}
+// void add_occurance(std::vector<std::vector<int>> &local_C, NumberType row_index, NumberType col_index, int myrank, int world_size)
+// {
+// 	// check owner
+// 	int destination = row_index % world_size;
+// 	if (destination == myrank)
+// 	{
+// 		// this rank own info on this row
+// 		local_C[row_index][col_index]++;
+// 	}
+// 	else
+// 	{
+// 		// send to others
+// 		NumberType buffer[2] = {row_index, col_index};
+// 		static_assert(std::is_same<NumberType, long>::value || std::is_same<NumberType, int>::value);
+// 		if (std::is_same<NumberType, long>::value)
+// 		{
+// 			// using num type = long
+// 			MPI_Send(buffer, 2, MPI_LONG, destination, 0, MPI_COMM_WORLD); // using tag = 0
+// 		}
+// 		else if (std::is_same<NumberType, int>::value)
+// 		{
+// 			// using num type = int
+// 			MPI_Send(buffer, 2, MPI_INT, destination, 0, MPI_COMM_WORLD);
+// 		}
+// 		else
+// 		{
+// 			std::cout << "NumberType is not a long or int\n";
+// 			exit(-1);
+// 		}
+// 	}
+// }
+
 
 std::vector<NumberType> selectSeed2D(std::vector<std::set<NumberType>> R, int k, NumberType num_node, int myrank, int world_size)
 {
@@ -80,6 +83,9 @@ std::vector<NumberType> selectSeed2D(std::vector<std::set<NumberType>> R, int k,
 	S.reserve(k);
 	std::vector<NumberType> count(num_node, 0);												// Count array
 	std::vector<std::vector<NumberType>> C(num_node, std::vector<NumberType>(num_node, 0)); // Co-occurrence matrix
+
+	std::unordered_map<int, std::vector<NumberType>> count_buffers;
+	std::unordered_map<int, std::vector<NumberType>> cooccur_buffers;
 
 	// Preprocessing step
 	size_t R_size = R.size();
@@ -93,7 +99,10 @@ std::vector<NumberType> selectSeed2D(std::vector<std::set<NumberType>> R, int k,
 		for (int j = 0; j < T_nodes.size(); j++)
 		{
 			// Count nodes in current RRR set
-			add_count(count, T_nodes[j], myrank, world_size);
+			int destination = T_nodes[j] % world_size;
+    	
+			// add_count(count, T_nodes[j], myrank, world_size);
+			add_count_batch(T_nodes[j], myrank, world_size, count_buffers, count);
 
 			for (int l = j; l < T_nodes.size(); l++)
 			{
@@ -101,15 +110,25 @@ std::vector<NumberType> selectSeed2D(std::vector<std::set<NumberType>> R, int k,
 				int b = T_nodes[l];
 
 				// Increment node pair co-occurences in current set
-				add_occurance(C, T_nodes[j], T_nodes[l], myrank, world_size);
+				// add_occurance(C, T_nodes[j], T_nodes[l], myrank, world_size);
+				add_occurance_batch(T_nodes[j], T_nodes[l], myrank, world_size,	cooccur_buffers, C);
 				if (a != b)
-					add_occurance(C, T_nodes[j], T_nodes[l], myrank, world_size);
+				add_occurance_batch(T_nodes[l], T_nodes[j], myrank, world_size,	cooccur_buffers, C);
+					// add_occurance(C, T_nodes[l], T_nodes[j], myrank, world_size);
 			}
 		}
 	}
 
 	// mpi barrier here
 	MPI_Barrier(MPI_COMM_WORLD);
+	flush_count_messages(count_buffers, get_mpi_type<NumberType>());
+	flush_occurance_messages(cooccur_buffers, get_mpi_type<NumberType>());
+
+
+	// now receive updates
+	receive_count_messages(count, myrank, get_mpi_type<NumberType>());
+	receive_occurance_messages(C, myrank, get_mpi_type<NumberType>());
+
 
 	// Seed Selection
 	int y_idx = -1;	  // Index of node with highest count
--- util.h
+++ util.h
@@ -2,27 +2,145 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <unordered_map>
+#include "EdgeInfo.h"
+typedef EdgeType::IntType NumberType;
+
+
 #include <mpi.h>
 
-template<typename T>
+
+
+template <typename T>
 MPI_Datatype get_mpi_type();
 
-template<>
+template <>
 MPI_Datatype get_mpi_type<int>() { return MPI_INT; }
 
-template<>
+template <>
 MPI_Datatype get_mpi_type<long>() { return MPI_LONG; }
 
-template<>
+template <>
 MPI_Datatype get_mpi_type<unsigned long>() { return MPI_UNSIGNED_LONG; }
 
-template<>
-MPI_Datatype get_mpi_type<size_t>() {
+template <>
+MPI_Datatype get_mpi_type<size_t>()
+{
     static_assert(sizeof(size_t) == sizeof(unsigned long), "Handle size_t mapping carefully!");
     return MPI_UNSIGNED_LONG;
 }
 
+
+
+//////////////// util used in selectSeed //////////////////////
+
+// function to buffer collection of message to be sent in bigger batch
+void add_count_batch(NumberType node_index,
+                     int myrank,
+                     int world_size,
+                     std::unordered_map<int, std::vector<NumberType>> &count_buffers,
+                     std::vector<NumberType> &local_count)
+{
+    int destination = node_index % world_size;
+    if (destination == myrank)
+    {
+        local_count[node_index]++;
+        return; 
+    }
+    count_buffers[destination].push_back(node_index);
+}
+
+// function to buffer collection of message to be sent in bigger batch
+void add_occurance_batch(NumberType row_index, NumberType col_index,
+                         int myrank,
+                         int world_size,
+                         std::unordered_map<int, std::vector<NumberType>> &cooccur_buffers,
+                         std::vector<std::vector<NumberType>> local_C)
+{
+    int destination = row_index % world_size;
+    if (destination == myrank)
+    {
+        // process locally
+        local_C[row_index][col_index]++;
+        return; 
+    }
+    cooccur_buffers[destination].push_back(row_index);
+    cooccur_buffers[destination].push_back(col_index);
+}
+
+
+void flush_count_messages(std::unordered_map<int, std::vector<NumberType>>& count_buffers,
+                          MPI_Datatype mpi_type)
+{
+    for (auto& [dest, buffer] : count_buffers) {
+        MPI_Send(buffer.data(), buffer.size(), mpi_type, dest, 0, MPI_COMM_WORLD);
+    }
+    count_buffers.clear();
+}
+
+
+void flush_occurance_messages(std::unordered_map<int, std::vector<NumberType>>& cooccur_buffers,
+                              MPI_Datatype mpi_type)
+{
+    for (auto& [dest, buffer] : cooccur_buffers) {
+        MPI_Send(buffer.data(), buffer.size(), mpi_type, dest, 1, MPI_COMM_WORLD);
+    }
+    cooccur_buffers.clear();
+}
+
+void receive_count_messages(std::vector<NumberType>& local_count, int myrank, MPI_Datatype mpi_type)
+{
+    MPI_Status status;
+    while (true) {
+        NumberType buffer[1024];
+        int flag;
+        MPI_Iprobe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &flag, &status);
+        if (!flag) break;
+
+        int count;
+        MPI_Get_count(&status, mpi_type, &count);
+        MPI_Recv(buffer, count, mpi_type, status.MPI_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        for (int i = 0; i < count; ++i) {
+            local_count[buffer[i]]++;
+        }
+    }
+}
+
+void receive_occurance_messages(std::vector<std::vector<int>>& local_C, int myrank, MPI_Datatype mpi_type)
+{
+    MPI_Status status;
+    while (true) {
+        NumberType buffer[1024];
+        int flag;
+        MPI_Iprobe(MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &flag, &status);
+        if (!flag) break;
+
+        int count;
+        MPI_Get_count(&status, mpi_type, &count);
+        MPI_Recv(buffer, count, mpi_type, status.MPI_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        for (int i = 0; i < count; i += 2) {
+            NumberType a = buffer[i];
+            NumberType b = buffer[i + 1];
+            local_C[a][b]++;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 #endif
+
 
 // example usage
 // MPI_Datatype mpi_type = get_mpi_type<NumberType>();
